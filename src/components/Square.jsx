44a62233637247e3bef8fdcb83d45cd20b37ebe5
salihslx <salihslislx@gmail.com>
2023-07-15 19:49:15 +0530

    installed sass. created board and square components barebones

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+const Square = ({ value }) => {
+
+    
+    return (
+    
+        <button type="button" className="square">
+        { value}</button> 
+            
+    )
+};
+export default Square;
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+ //main.jsx is the root file configuration in index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //main component in react app is <App/>
+  /* 1) .component start capital latter
+    2) .component is colsed  */
   <React.StrictMode>
     <App />
   </React.StrictMode>,
